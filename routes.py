152857eb5b002d5dbe095fb67c3c6e4bff1cdd04
aritchie <alex.ritchie@non-von.com>
2024-11-11 19:20:13 -0500

    basic working version of the website. Going to put it up and add projects/blogs soon.

--- routes.py
+++ routes.py
@@ -9,17 +9,17 @@ def home():
 def about():
     return render_template('sections/about.html')
 
-@app.route('/projects')
-def projects():
-    return render_template('sections/projects.html')
+# @app.route('/projects')
+# def projects():
+#     return render_template('sections/projects.html')
 
 @app.route('/publications')
 def publications():
     return render_template('sections/publications.html')
 
-@app.route('/blog')
-def blog():
-    return render_template('sections/blog.html')
+# @app.route('/blog')
+# def blog():
+#     return render_template('sections/blog.html')
 
 @app.route('/contact')
 def contact():
